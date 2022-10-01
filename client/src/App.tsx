4442ce04fecf9603c7c31e9c45115cc5a4d02174
saspoon1 <saspoon@emory.edu>
2022-10-01 14:22:34 -0400

    Frontend register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import styles from "./App.module.scss";
 import Login from "./pages/Login";
+import Register from "./pages/Register"
 
 const App = () => {
   return (
     <div className={styles["App"]}>
+      // Comment out or delete <Register/> out to see login.
+      <Register/>
       <Login />
     </div>
   );
